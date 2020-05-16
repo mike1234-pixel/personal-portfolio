6ec2363c409ec0352280e3c356be81dba8b22440
Michael Tandy <mike.tandy.1@gmail.com>
2020-05-16 17:44:52 +0100

    built/added custom drop down menu for navbar.

---
+++ src/Components/Dropdown.jsx
@@ -0,0 +1,146 @@
+import React from "react";
+
+class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuClassName: "initialState",
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.menuClassName === "initialState") {
+      this.setState({
+        menuClassName: "visible",
+      });
+    } else if (this.state.menuClassName === "visible") {
+      this.setState({
+        menuClassName: "invisible",
+      });
+    } else {
+      this.setState({
+        menuClassName: "visible",
+      });
+    }
+    console.log(this.state.menuClassName);
+  }
+
+  handleClickOutside(e) {
+    if (
+      e.target.id !== "dropdown-menu" &&
+      e.target.className !== "nav-item" &&
+      e.target.className !== "nav-link" &&
+      e.target.className !== "dropdown-button"
+    ) {
+      if (this.state.menuClassName === "visible") {
+        this.setState({
+          menuClassName: "invisible",
+        });
+      }
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside);
+  }
+
+  componentWillReceiveProps() {
+    if (this.props.hamburgerOpen === false) {
+      this.setState(() => ({
+        menuClassName: "initialState",
+      }));
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.handleClickOutside);
+  }
+
+  // click event checks whether target is not div
+
+  render() {
+    return (
+      <div className="dropdown-container">
+        <button className="dropdown-button" onClick={this.handleClick}>
+          Apps
+        </button>
+        <ul className={this.state.menuClassName} id="dropdown-menu">
+          <li className="nav-item">
+            <a
+              href="https://mike1234-pixel.github.io/LOTRQuote/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              Random Quote Machine
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://mike1234-pixel.github.io/MarkdownPreviewer/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              Markdown Previewer
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://mike1234-pixel.github.io/Drums/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              Drum Machine
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://mike1234-pixel.github.io/JSCalculator/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              JS Calculator
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://master.d2icos6v1wkh08.amplifyapp.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              Pomodoro Clock
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://mike1234-pixel.github.io/Simon-Game/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              Simon Game
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              href="https://mike1234-pixel.github.io/SeeLondon-Project/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="nav-link"
+            >
+              SeeLondon.co.uk
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,100 +1,130 @@
 import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Dropdown from "./Dropdown.jsx";
 
-function Navigation() {
-  return (
-    <Navbar collapseOnSelect expand="lg" variant="dark" id="nav">
-      <Navbar.Brand id="navbar-title">
-        <Link to="/" style={{ color: "inherit", textDecoration: "inherit" }}>
-          Mike Tandy Dev
-        </Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <NavDropdown
-            title="Apps"
-            id="collasible-nav-dropdown"
-            className="sans-serif-font"
-          >
-            <NavDropdown.Item
-              href="https://mike1234-pixel.github.io/Simon-Game/"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              Simon Game
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              href="https://ancient-stream-79404.herokuapp.com/"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              Newsletter SignUp
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              href="https://mike1234-pixel.github.io/Drum-Machine/"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              Drum Machine
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              href="https://immense-spire-07308.herokuapp.com/"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              To-do List
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              href="https://github.com/mike1234-pixel/REST-API/blob/master/app.js"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              RESTful API
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              href="https://master.d2icos6v1wkh08.amplifyapp.com/"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              Pomodoro Clock
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item
-              href="https://mike1234-pixel.github.io/SeeLondon-Project/"
-              target="_blank"
-              className="sans-serif-font"
-            >
-              SeeLondon.com
-            </NavDropdown.Item>
-          </NavDropdown>
-          <Nav.Link className="sans-serif-font">
+class Navigation extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      hamburgerOpen: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+      hamburgerOpen: !prevState.hamburgerOpen,
+    }));
+    console.log(this.state.hamburgerOpen);
+  }
+  render() {
+    return (
+      <div>
+        <Navbar collapseOnSelect expand="lg" variant="dark" id="nav">
+          <Navbar.Brand id="navbar-title">
             <Link
-              to="/AboutMe"
-              style={{ color: "inherit", textDecoration: "inherit" }}
-              className="sans-serif-font"
-            >
-              About Me
-            </Link>
-          </Nav.Link>
-          <Nav.Link className="sans-serif-font">
-            <Link
-              to="/Contact"
+              to="/"
               style={{ color: "inherit", textDecoration: "inherit" }}
             >
-              Contact
+              Mike Tandy Dev
             </Link>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+          </Navbar.Brand>
+
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            onClick={this.handleClick}
+          />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Dropdown hamburgerOpen={this.state.hamburgerOpen} />
+            <Nav className="mr-auto">
+              <Nav.Link className="sans-serif-font">
+                <Link
+                  to="/AboutMe"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                  className="sans-serif-font"
+                >
+                  About Me
+                </Link>
+              </Nav.Link>
+              <Nav.Link className="sans-serif-font">
+                <Link
+                  to="/Contact"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                >
+                  Contact
+                </Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
+
+{
+  /* <NavDropdown
+title="Apps"
+id="collasible-nav-dropdown"
+className="sans-serif-font"
+>
+<NavDropdown.Item
+  href="https://mike1234-pixel.github.io/Simon-Game/"
+  target="_blank"
+  className="sans-serif-font"
+>
+  Simon Game
+</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item
+  href="https://ancient-stream-79404.herokuapp.com/"
+  target="_blank"
+  className="sans-serif-font"
+>
+  Newsletter SignUp
+</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item
+  href="https://mike1234-pixel.github.io/Drum-Machine/"
+  target="_blank"
+  className="sans-serif-font"
+>
+  Drum Machine
+</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item
+  href="https://immense-spire-07308.herokuapp.com/"
+  target="_blank"
+  className="sans-serif-font"
+>
+  To-do List
+</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item
+  href="https://github.com/mike1234-pixel/REST-API/blob/master/app.js"
+  target="_blank"
+  className="sans-serif-font"
+>
+  RESTful API
+</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item
+  href="https://master.d2icos6v1wkh08.amplifyapp.com/"
+  target="_blank"
+  className="sans-serif-font"
+>
+  Pomodoro Clock
+</NavDropdown.Item>
+<NavDropdown.Divider />
+<NavDropdown.Item
+  href="https://mike1234-pixel.github.io/SeeLondon-Project/"
+  target="_blank"
+  className="sans-serif-font"
+>
+  SeeLondon.com
+</NavDropdown.Item>
+</NavDropdown> */
+}
